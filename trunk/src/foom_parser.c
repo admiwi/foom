ad6bbc6b726ee954d0f910c34a5a7eea8eeb94bd
admiwi <admiwi@c499c401-2953-0410-b2c1-0b640bbdf8ce>
2008-10-13 10:52:46 +0000

    lots of changes, working towards parser...

---
+++ trunk/src/foom_parser.c
@@ -0,0 +1,10 @@
+#include "foom.h"
+#include "foom_lex.h"
+
+// _if_statement_ = if <exp> <closure> [else _if_statement`_]
+
+
+void sym_integer(parse_pkg * pp, ast * st, scope * s, token * t) {
+}
+void sym_string(parse_pkg * pp, ast * st, scope * s, token * t) {
+}
