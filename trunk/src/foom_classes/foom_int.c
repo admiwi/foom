37d6b1915bb2a14012a8f7f7bc140382758be9a8
admiwi <admiwi@c499c401-2953-0410-b2c1-0b640bbdf8ce>
2008-12-01 03:08:31 +0000

    added slash/div

--- trunk/src/foom_classes/foom_int.c
+++ trunk/src/foom_classes/foom_int.c
@@ -5,45 +5,34 @@
 SYMBOLS;
 map * native_classes;
 
+object * int_mult(object * self, object * arg) {
+  object * tmp, * o;
+  o = new_int();
+  o->val.Int = self->val.Int * arg->val.Int;
+  return o;
+}
+
+object * int_div(object * self, object * arg) {
+  object * tmp, * o;
+  o = new_int();
+  o->val.Int = self->val.Int / arg->val.Int;
+  return o;
+}
+
 object * int_plus(object * self, object * arg) {
   object * tmp, * o;
-  if(self->type != arg->type) {
-    fprintf(stderr, "should deal with invalid types\n");
-    //mebbe an exception object?
-    /*
-    tmp = get_member(arg->members, "_to_int_");
-    if(tmp && tmp->type == func_sym) {
-      //list * args = new_list();
-
-    }else {
-
-    }*/
-  }
   o = new_int();
-  //o->type = int_sym;
   o->val.Int = self->val.Int + arg->val.Int;
   return o;
 }
 
 object * int_minus(object * self, object * arg) {
   object * tmp, * o;
-  if(self->type != arg->type) {
-    fprintf(stderr, "should deal with invalid types\n");
-    //mebbe an exception object?
-    /*
-    tmp = get_member(arg->members, "_to_int_");
-    if(tmp && tmp->type == func_sym) {
-      //list * args = new_list();
-
-    }else {
-
-    }*/
-  }
   o = new_int();
-  //o->type = int_sym;
   o->val.Int = self->val.Int - arg->val.Int;
   return o;
 }
+
 object * int_compare(object * self, object * arg) {
   object * o = new_int();
   if(self->val.Int < arg->val.Int) o->val.Int = -1;
@@ -75,6 +64,8 @@ object * int_class() {
   o->null = false;
   o->name = "int";
   map_set(native_classes, o->name, o, map_object|map_immutable);
+  add_member_name(o, native_wrapper(&int_mult, func_binary), _symbols_[star_sym]);
+  add_member_name(o, native_wrapper(&int_div, func_binary), _symbols_[slash_sym]);
   add_member_name(o, native_wrapper(&int_minus, func_binary), _symbols_[minus_sym]);
   add_member_name(o, native_wrapper(&int_plus, func_binary), _symbols_[plus_sym]);
   add_member_name(o, native_wrapper(&int_to_string, func_unary), "to_string");
