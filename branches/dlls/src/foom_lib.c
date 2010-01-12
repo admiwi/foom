b775314112b3408ce1ab2c905bbd550204ee292b
admiwi <admiwi@c499c401-2953-0410-b2c1-0b640bbdf8ce>
2010-01-12 12:00:43 +0000

    moving stuff around need to get rid of native_classes global and rely on global scope...

---
+++ branches/dlls/src/foom_lib.c
@@ -0,0 +1,23 @@
+#include "foom.h"
+#include "foom_lib.h"
+#include <windows.h>
+typedef void (__cdecl *NATIVE_LIB)(scope*, map*);
+void init_sys_lib(scope*);
+
+int init_native_lib(char * n, scope * scp){
+  HINSTANCE hinstLib; 
+  NATIVE_LIB n_init; 
+  hinstLib = LoadLibrary(n); 
+ 
+  if(hinstLib) { 
+    n_init = (NATIVE_LIB) GetProcAddress(hinstLib, "init_lib"); 
+    if(n_init) 
+       (n_init)(scp, native_classes);
+    else return 0;
+  } else return 0;
+  return 1;
+}
+
+void init_libs(scope * cscope) {
+  printf("%d\n", init_native_lib("dlls\\foom_sys.dll", cscope));
+}
--- branches/dlls/src/foom_libs/foom_lib.c
+++
@@ -1,8 +0,0 @@
-#include "foom.h"
-#include "foom_lib.h"
-
-void init_sys_lib(scope*);
-
-void init_libs(scope * cscope) {
-  init_sys_lib(cscope);
-}
